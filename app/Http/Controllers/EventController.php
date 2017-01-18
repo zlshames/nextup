c6c7080d1ca5a32e839646b69d03e268b7360cd3
vagrant <vagrant@homestead>
2017-01-18 22:46:18 +0000

    event stuff

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,30 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Larapi;
+
+use App\Http\Event;
 
 class EventController extends Controller
 {
-    // POST
-    public function store(Request $request)
-    {
+		// POST
+		public function store(Request $request)
+		{
+				$user = Auth::user();
+				if ($user === NULL) {
+						return Larapi::forbidden();
+				}
 
-    }
+				// Do logic here
+		}
 
-    // GET
-    public function show(Request $request, $id)
-    {
+		// GET
+		public function show(Request $request, $id)
+		{
+				// I'm not 100% sure, but try:
+				// Event::all();
+		}
 
-    }
+		// GET
+		public function showAll(Request $request)
+		{
 
-    // PUT
-    public function update(Request $request, $id)
-    {
+		}
 
-    }
+		// PUT
+		public function update(Request $request, $id)
+		{
 
-    // DELETE
-    public function destroy(Request $request, $id)
-    {
+		}
 
-    }
+		// DELETE
+		public function destroy(Request $request, $id)
+		{
+
+		}
 }
