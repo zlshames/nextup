90d61bb39e1acd51e0bddf36f3fce6a1062a5baa
Zach Shames <zlshames@gmail.com>
2017-01-19 00:42:25 -0500

    switched back to web auth purely because api routes add a 'auth' prefix that we do not want

--- routes/api.php
+++ routes/api.php
@@ -12,26 +12,3 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/', 'HomeController@index');
-
-Route::group(['prefix' => 'auth'], function () {
-	Route::post('signup', 'AuthController@signup');
-	Route::post('signin', 'AuthController@signin');
-	Route::post('signout', 'AuthController@signout');
-	Route::get('check', 'AuthController@isAuthenticated');
-});
-
-Route::group(['prefix' => 'v1'], function () {
-	Route::resource('events', 'UserController', ['only' => [
-		'show', 'update', 'destroy'
-	]]);
-
-	Route::resource('events', 'EventController', ['only' => [
-		'store', 'show', 'update', 'destroy'
-	]]);
-
-	Route::resource('categories', 'CategoryController', ['only' => [
-		'store', 'show', 'update', 'destroy'
-	]]);
-});
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', 'HomeController@index');
+
+Route::group(['prefix' => 'api/auth'], function () {
+	Route::post('signup', 'AuthController@signup');
+	Route::post('signin', 'AuthController@signin');
+	Route::post('signout', 'AuthController@signout');
+	Route::get('check', 'AuthController@isAuthenticated');
+});
+
+Route::group(['prefix' => 'api/v1'], function () {
+	Route::resource('events', 'UserController', ['only' => [
+		'show', 'update', 'destroy'
+	]]);
+
+	Route::resource('events', 'EventController', ['only' => [
+		'store', 'show', 'update', 'destroy'
+	]]);
+
+	Route::resource('categories', 'CategoryController', ['only' => [
+		'store', 'show', 'update', 'destroy'
+	]]);
+});
