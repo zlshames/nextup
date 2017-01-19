ef5551882880dcfdc579018e58ca3dc5e1ba30e2
Anthony Michelizzi <cynical89@gmail.com>
2017-01-18 16:52:09 -0800

    Events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,35 +18,86 @@ class EventController extends Controller
 						return Larapi::forbidden();
 				}
 
-				// Do logic here
+				if (!$request->name || !$request->start || !$request->category_id) {
+					return Larapi::badRequest();
+				}
+
+				$event = new Event;
+
+				$event->name = $request->name;
+				$event->start = $request->start;
+				$event->category_id =  $request->category_id;
+
+				if($request->finish) {
+					$event->finish = $request->finish;
+				}
+
+				$event->save();
+				return Larapi::created($event);
 		}
 
 		// GET
-		public function show(Request $request, $id)
+		public function show($id)
 		{
-				if ($id == "all") {
-					$events = Event::all();
-					if (sizeof($events) > 0) {
-						return Larapi::ok($events);
-					}
-					return Larapi::noContent();
+			if($id == null) {
+				return Larapi::badRequest();
+			}
+
+			if ($id == "all") {
+				$events = Event::all();
+				if (sizeof($events) > 0) {
+					return Larapi::ok($events);
 				}
-				$event = Event::find($id);
-				if (sizeof($event) > 0) {
-					return Larapi::noContent();
-				}
+
 				return Larapi::noContent();
+			}
+
+			if (!is_numeric($id)) {
+				return Larapi::badRequest();
+			}
+
+			$event = Event::find($id);
+			if (sizeof($event) > 0) {
+				return Larapi::noContent();
+			}
+			
+			return Larapi::noContent();
 		}
 
 		// PUT
 		public function update(Request $request, $id)
 		{
+			if($id == null) {
+				return Larapi::badRequest();
+			}
 
+			$event = Event::find($id);
+
+			if ($request->name != null) {
+			$event->name = $request->name;
+			}
+			if ($request->start != null) {
+			$event->start = $request->start;
+			}
+			if ($request->finish != null) {
+			$event->finish = $request->finish;
+			}
+			if ($request->category_id != null) {
+			$event->category_id = $request->category_id;
+			}
+
+			$event->save();
+			return Larapi::accepted($event);
 		}
 
 		// DELETE
-		public function destroy(Request $request, $id)
+		public function destroy($id)
 		{
+			if($id == null) {
+				return Larapi::badRequest();
+			}
 
+			$event = Event::find($id);
+			$event->delete();
 		}
 }
